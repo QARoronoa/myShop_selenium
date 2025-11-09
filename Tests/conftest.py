047549185e4dc04851160316a58d2ee206df31f4
issa Kamara <issa.kamara@ymail.com>
2025-11-09 16:55:58 +0100

    premier commit

---
+++ Tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+@pytest.fixture(scope='function')
+def setup():
+    options = Options()
+    driver = webdriver.Firefox(options=options)
+    driver.get('http://www.automationpractice.pl/index.php')
+    driver.implicitly_wait(5000)
+    driver.close()
